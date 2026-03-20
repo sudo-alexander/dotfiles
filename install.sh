e9e981e94063787e46d588a6f854dcae3d738b08
sasha <adk700@yandex.ru>
2026-03-21 01:57:29 +0300

    refactor: rewrite install script for GNU stow

--- install.sh
+++ install.sh
@@ -1,58 +1,23 @@
 #!/bin/bash
 
-DOTFILES=("firefox configs" ".zshrc" ".vimrc")
+set -eo pipefail
+
 echo "Hi!"
-read -p "Choose your dotfile (just select the number, default=ALL): 1 - firefox configs; 2 - .zshrc 3 - .vimrc" SELECTION
-if [[ -z $SELECTION ]]; then
-    SELECTION="1 2 3"
-fi
+DOTFILES=("zsh" "vim" "alacritty" "tmux")
 
-for CHOICE in $SELECTION; do
-    ITEM="${DOTFILES[((CHOICE - 1))]}"
-    if [[ "$ITEM" == "firefox configs" ]]; then
-        PATH_TO_USER_FF_PROFILE="$(find "$HOME/.mozilla/firefox" -name "*default-release*")"
-        if [[ ! -d "${PATH_TO_USER_FF_PROFILE}/chrome" ]]; then
-            read -p "There's no chrome directory in your firefox profile directory, \
-                it's required for applying configs. Do you want to create it? [y/N]" CONFIRM
-            if [[ "${CONFIRM,,}" == "y" ]]; then
-                echo "Creating chrome directory..." && mkdir "$PATH_TO_USER_FF_PROFILE/chrome" && echo "Done!"
-            else
-                echo "Couldn't install firefox configs: process interrupted by user"
-                exit 1
-            fi
-        fi
+# install required packages
+sudo pacman -S --needed stow zsh gvim alacritty tmux wl-clipboard
 
-        if [[ -d "${PATH_TO_USER_FF_PROFILE}/chrome" ]]; then
-            echo "Enabling CSS support in Firefox preferences..." \
-            && echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> "${PATH_TO_USER_FF_PROFILE}/user.js"
-            CSS_FILES=("userChrome.css" "userContent.css")
-            for CONFIG in "${CSS_FILES[@]}"; do
-                if [[ -f "${PATH_TO_USER_FF_PROFILE}/chrome/$CONFIG" ]]; then
-                    read -p "file "$CONFIG" already exists. Do you want to create a backup and replace it? [y/N]" CONFIRM
-                    if [[ ${CONFIRM,,} == "y" ]];then
-                        echo "Creating $CONFIG.bak..." \
-                        && mv "${PATH_TO_USER_FF_PROFILE}/chrome/$CONFIG" "${PATH_TO_USER_FF_PROFILE}/chrome/$CONFIG.bak" \
-                        && ln -sf "$(pwd)/firefox/$CONFIG" "$PATH_TO_USER_FF_PROFILE/chrome/$CONFIG" \
-                        && echo "Done!"
-                    else
-                        echo "Couldn't create $CONFIG: process interrupted by user"
-                    fi
-                else
-                    ln -sf "$(pwd)/firefox/$CONFIG" "$PATH_TO_USER_FF_PROFILE/chrome/$CONFIG"
-                fi
-            done
-        fi
-
-    else
-        if [[ -f "$HOME/$ITEM" ]]; then
-            read -p "$ITEM is already exists. Do you want to make a backup and replace it? [y/N]" CONFIRM
-            if [[ "${CONFIRM,,}" == "y" ]]; then
-                mv "$HOME/$ITEM" "$HOME/$ITEM.bak" \
-                && ln -sf "$(find $(pwd) -name $ITEM)" "$HOME/$ITEM" \
-                && echo "Done."
-            fi
-        else
-            ln -sf "$(pwd)/$ITEM" "$HOME/$ITEM"
-        fi
-    fi
+# create dotfiles symlinks in system
+for ITEM in "${DOTFILES[@]}"; do
+    stow $ITEM
 done
+
+# configure firefox
+FF_USER_PROFILE="$(find "$HOME" -type d -name "*.default-release*" 2>/dev/null | grep -i -v "cache" | head -n 1)"
+mkdir -p "$FF_USER_PROFILE/chrome"
+RES_PATH="$FF_USER_PROFILE/chrome"
+ln -sf "$(pwd)/firefox/userContent.css" "$RES_PATH/userContent.css"
+ln -sf "$(pwd)/firefox/userChrome.css" "$RES_PATH/userChrome.css"
+echo "Set 'toolkit.legacyUserProfileCustomizations.stylesheets' to true"
+echo "Done."

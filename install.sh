a9ee676312f0b93c167d65737212bee2fb87b7e0
sasha-archlinux-pc <adk700@yandex.ru>
2026-03-21 15:59:46 +0300

    feat(install): make script available on fedora and debian-like distros

--- install.sh
+++ install.sh
@@ -5,8 +5,32 @@ set -eo pipefail
 echo "Hi!"
 DOTFILES=("zsh" "vim" "alacritty" "tmux")
 
+# define your distro
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$ID
+fi
+
 # install required packages
-sudo pacman -S --needed stow zsh gvim alacritty tmux wl-clipboard
+case $OS in
+    arch|manjaro)
+        INSTALL_CMD="sudo pacman -S --needed"
+        PACKAGES="stow zsh gvim alacritty tmux wl-clipboard"
+        ;;
+    ubuntu|debian|pop)
+        INSTALL_CMD="sudo apt update && sudo apt install -y"
+        PACKAGES="stow zsh vim-gtk3 alacritty tmux wl-copy" 
+        ;;
+    fedora)
+        INSTALL_CMD="sudo dnf install -y"
+        PACKAGES="stow zsh vim-X11 alacritty tmux wl-clipboard"
+        ;;
+    *)
+        echo "Unknown OS: $OS. Install packages manually."
+        exit 1
+        ;;
+esac
+$INSTALL_CMD $PACKAGES
 
 # create dotfiles symlinks in system
 for ITEM in "${DOTFILES[@]}"; do
@@ -14,10 +38,19 @@ for ITEM in "${DOTFILES[@]}"; do
 done
 
 # configure firefox
+cd firefox
 FF_USER_PROFILE="$(find "$HOME" -type d -name "*.default-release*" 2>/dev/null | grep -i -v "cache" | head -n 1)"
-mkdir -p "$FF_USER_PROFILE/chrome"
+mkdir -p "$FF_USER_PROFILE/chrome/img"
 RES_PATH="$FF_USER_PROFILE/chrome"
-ln -sf "$(pwd)/firefox/userContent.css" "$RES_PATH/userContent.css"
-ln -sf "$(pwd)/firefox/userChrome.css" "$RES_PATH/userChrome.css"
+cp -a "$(pwd)/userContent.css" "$RES_PATH/."
+cp -a "$(pwd)/userChrome.css" "$RES_PATH/."
+cp -a "$(pwd)/img/dark-gradient-background.jpg" "$RES_PATH/img"
 echo "Set 'toolkit.legacyUserProfileCustomizations.stylesheets' to true"
+cd ..
+
+if [ "$SHELL" != "$(which zsh)" ]; then
+    echo "Changing shell to zsh..."
+    chsh -s $(which zsh)
+fi
+
 echo "Done."
